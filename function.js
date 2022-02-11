798600d4162dab07643cddf3c50a543177797a9a
ruby78297 <rrubyrana8@gmail.com>
2022-02-11 15:12:12 +0530

    fruit juice processor

--- function.js
+++ function.js
@@ -1,6 +1,19 @@
 // function
+//basic function 
 
 function myFirstFunction() {
     console.log("hi this is my my function")
 }
 myFirstFunction()
+
+
+//juice processor
+
+function juiceProcessor(apple, orange) {
+    const juice = `Juice with ${apple}  apples and ${orange} oranges `;
+    return juice;
+}
+
+const juice = juiceProcessor(2, 3);
+console.log(juice)
+
